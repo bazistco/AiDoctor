e8fb3c9613d9509360984e0fae19b6aa3a0af98a
salman rajabi <77535863+dastanrab@users.noreply.github.com>
2026-08-31 11:23:27 +0330

    add softdelete

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ namespace App\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -29,7 +30,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  */
 class User extends Authenticatable
 {
-    use HasApiTokens;
+    use HasApiTokens, SoftDeletes;
 
 	protected $table = 'users';
 
