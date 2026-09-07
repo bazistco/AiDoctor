2dcb7458d2ceb655d770d2973a6ecf3300ce6a91
salman rajabi <77535863+dastanrab@users.noreply.github.com>
2026-09-07 10:36:23 +0330

    test kave

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Api\Controllers\Auth\AuthController;
 use App\Http\Controllers\Api\DiagnosisController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\ChatController;
@@ -15,10 +16,22 @@ use App\Http\Controllers\Api\Admin\AppointmentManagementController;
 use App\Http\Controllers\Api\ReviewController;
 use App\Http\Controllers\Api\MedicalServiceProviderController;
 use App\Http\Controllers\Api\Admin\AdminServiceController;
+use Kavenegar\KavenegarApi;
 
 
 
 
+
+Route::get('/test-sms',function (){
+
+    try {
+        $k = new KavenegarApi(env('KAVENEGAR_API_KEY'));
+        $k->VerifyLookup('09388985617','test',"","",'welcome','sms');
+    }catch (\Exception $e){
+         dd($e->getMessage(),env('KAVENEGAR_API_KEY'));
+    }
+
+});
 Route::get('/health',function (){
     return response()->json(["status"=>"success","data"=>['date'=>now()]]);
 });
