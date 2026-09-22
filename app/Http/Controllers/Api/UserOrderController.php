d8922c06e2f6236f9a4cac584ae421033054fabc
salman rajabi <77535863+dastanrab@users.noreply.github.com>
2026-09-22 11:19:52 +0330

    pg

--- app/Http/Controllers/Api/UserOrderController.php
+++ app/Http/Controllers/Api/UserOrderController.php
@@ -160,7 +160,7 @@ class UserOrderController extends Controller
         $q2 = DB::table('users_labs_requests as ulr')
             ->join('labs_info as li', 'ulr.lab_id', '=', 'li.user_id')
             ->leftJoin('orders as o', function ($join) use ($userId) {
-                $join->on('o.reason_ref', '=', DB::raw('CAST(sa.id AS CHAR)'))
+                $join->on('o.reason_ref', '=', DB::raw('CAST(ulr.id AS CHAR)'))
                     ->where('o.reason_id', '=', 5)
                     ->where('o.user_id', '=', $userId)
                     ->where('o.status', '=', 1); // سفارشات با درگاه باز/در انتظار پرداخت
